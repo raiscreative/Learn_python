20a85d97325030a65570200eca7bfa0fe1a74fc4
Raluca Drăghicescu <64273721+raiscreative@users.noreply.github.com>
2021-02-24 17:53:44 +0200

    Add files via upload

---
+++ lpthw/ex19.py
@@ -0,0 +1,33 @@
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    print(f'You have {cheese_count} cheeses!')
+    print(f'You have {boxes_of_crackers} boxes of crackers!')
+    print("Man that's enough for a party!")
+    print('Get a blanket. \n')
+
+def my_func(*args):
+    for arg in args:
+        print(f'Once upon a time there was a {arg} in a code.')
+
+print('We can just give the function numbers dirctly:')
+cheese_and_crackers(20, 30)
+
+print('OR, we can use variables from our script:')
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+print('We can even do math inside too:')
+cheese_and_crackers(10 + 20, 5 + 6)
+
+print('And we can combine the two, variables and math:')
+cheese_and_crackers(amount_of_cheese +100, amount_of_crackers + 1000)
+
+my_func(5)
+my_func(6, 9, 3)
+my_func(5 + 5)
+my_func('bug', 'butterfly', 'ant')
+your_choice = input('What do you think it is all about? ')
+my_func(your_choice)
+my_func('whatever')
+my_func('"that should be enough, don\'t you find?"')
